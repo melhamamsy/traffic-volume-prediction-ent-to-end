bffded974a8b9923421a53dc4884ccddb38f2883
melhamamsy <elhamamsy.m.a@gmail.com>
2024-07-16 22:01:21 +0300

    Created docker images with docker compose for grafana/postgres/adminer

---
+++ monitoring/utils/data_preparation/cleaning.py
@@ -0,0 +1,30 @@
+from pandas import DataFrame, to_datetime
+
+
+def correct_dtypes(
+    df: DataFrame,
+    **kwargs
+) -> DataFrame:
+    """
+    Correct dtypes of the columns of the DataFrame
+    """
+
+    data_time_column = kwargs["date_time_column"]
+    df[data_time_column] = to_datetime(df[data_time_column])
+
+    df["temp"] = df["temp"].astype(float)
+    df["rain_1h"] = df["rain_1h"].astype(float)
+    df["snow_1h"] = df["snow_1h"].astype(float)
+    df["clouds_all"] = df["clouds_all"].astype(float)
+
+    return df
+
+
+def remove_duplicates(
+    df: DataFrame,
+) -> DataFrame:
+    """
+    Drops duplicates of the DataFrame
+    """
+
+    return df.drop_duplicates(keep='last').reset_index(drop=True)
---
+++ monitoring/utils/data_preparation/encoders.py
@@ -0,0 +1,83 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction import DictVectorizer
+
+from pandas import DataFrame, Series
+
+
+class DictVectorizerTransformer(BaseEstimator, TransformerMixin):
+    """
+    A class to one-hot-encode the string columns, used as a step in the pipeline
+
+    Attributes:
+    -----------
+    vectorizer : DictVectorizer
+        Scikit-learn dict-vectorizer
+    
+    sparse : bool
+        A boolean to decide whether or not to produce a sparse output
+    """
+
+    def __init__(self, sparse=False):
+        """
+        Initialize the DictVectorizerTransformer with the vectorizer and sparse bool.
+
+        Parameters:
+        -----------
+        sparse : bool
+            A boolean to decide whether or not to produce a sparse output
+        """
+        self.sparse = sparse
+        self.vectorizer = DictVectorizer(sparse=self.sparse)
+
+    def fit(
+        self, 
+        X: DataFrame,
+        y: Series = None
+    ) -> "DictVectorizerTransformer":
+        """
+        Fit the vectorizer to the training data
+
+        Parameters:
+        -----------
+        X : DataFrame
+            X features
+
+        y : Series
+            y features
+
+        Returns:
+        --------
+        DictVectorizerTransformer
+            The DictVectorizerTransformer object itself
+        """
+        self.vectorizer.fit(X[['weather_main']].to_dict(orient='records'))
+        
+        return self
+
+    def transform(
+        self, 
+        X: DataFrame,
+        y: Series = None
+    ) -> DataFrame:
+        """
+        Transform data using the pre-fit vectorizer
+
+        Parameters:
+        -----------
+        X : DataFrame
+            X features
+
+        y : Series
+            y features
+
+        Returns:
+        --------
+        DataFrame
+            Transformed data
+        """
+        weather_main_transformed = self.vectorizer.transform(X[['weather_main']].to_dict(orient='records'))
+        weather_main_df = DataFrame(weather_main_transformed, 
+                                       columns=self.vectorizer.get_feature_names_out(), 
+                                       index=X.index)
+        X_transformed = X.join(weather_main_df).drop(columns=['weather_main'])
+        return X_transformed
